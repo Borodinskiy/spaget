2dc3d6ae245e7c74f37ab135e9f91889779efeb2
Borodinskiy <ivanforest856@gmail.com>
2022-08-14 17:13:50 +0300

    UITexture added, not finished, in process

--- code/button.hpp
+++ code/button.hpp
@@ -1,6 +1,27 @@
 #pragma once
 #include "uiBase.hpp"
 
+class UISprite : public sf::Drawable
+{
+public:
+    UISprite(sf::Texture& texture, sf::Vector2u tilesize);
+    ~UISprite();
+
+    void initialize();
+    void setSize(sf::Vector2f s);
+
+private:
+    void draw(sf::RenderTarget &target, sf::RenderStates states) const;
+
+private:
+    sf::Image m_image;
+    sf::Texture& m_tex;
+    sf::VertexArray m_fone;
+
+    sf::Vector2u m_tilesize;
+    std::list<sf::IntRect> m_tilerects;
+};
+
 class UIButton : public UI
 {
 public:
